e9ac12188ec72bf106bd6a8c0929edaccca924bf
Rossler Boquiren <rgkaboquiren@gmail.com>
2026-04-19 00:21:15 -0400

    initialize

---
+++ script.js
@@ -0,0 +1,48 @@
+// Build it from scratch
+// generate two random passwords when the user clicks the button
+// each password should be 15 characters long
+
+const characters = [
+	"A","B","C","D","E","F","G","H","I",
+	"J","K","L","M","N","O","P","Q","R",
+	"S","T","U","V","W","X","Y","Z","a",
+	"b","c","d","e","f","g","h","i","j",
+	"k","l","m","n","o","p","q","r","s",
+	"t","u","v","w","x","y","z", "0", "1", 
+	"2", "3", "4", "5", "6", "7", "8", "9",
+	"~","`","!","@","#","$","%","^","&","*",
+	"(",")","_","-","+","=","{","[","}","]",
+	",","|",":",";","<",">",".","?","/"
+];
+
+let passwordLength = 15;
+let passwordOneEl = document.getElementById('passwordOne-el');
+let passwordTwoEl = document.getElementById('passwordTwo-el');
+
+function generatePassword() {
+
+	let passwordOne = '';
+	let passwordTwo = '';
+
+	for (let i = 0; i < passwordLength; i++) {
+		passwordOne += characters[Math.floor(Math.random() * characters.length)];
+		passwordTwo += characters[Math.floor(Math.random() * characters.length)];
+	}
+
+	passwordOneEl.textContent = passwordOne;
+	passwordTwoEl.textContent = passwordTwo;
+
+		// for (let i = 0; i < passwordLength; i++) {
+		// indexOne.push( Math.floor(Math.random() * characters.length) );
+		// indexTwo.push( Math.floor(Math.random() * characters.length) );
+		// }
+		// for (let i = 0; i < indexOne.length; i++) {
+		// 	passwordOne += characters[indexOne[i]];
+		// 	passwordTwo += characters[indexTwo[i]];
+		// }
+		
+		// passwordOneEl.textContent = passwordOne;
+		// passwordTwoEl.textContent = passwordTwo;
+
+}
+		
